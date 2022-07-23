46ac73142f0868318c8fa69fc47274dc16a33a5a
Avanti Dorle <avantidorle@avantis-air.myfiosgateway.com>
2022-07-23 17:28:04 -0400

    Checking database connectivity

---
+++ app/src/main/java/edu/neu/madcourse/cs5520_finalproject_team26/models/User.java
@@ -0,0 +1,12 @@
+package edu.neu.madcourse.cs5520_finalproject_team26.models;
+
+public class User {
+
+    public String username;
+    public String userId;
+
+    public User(String username, String userId) {
+        this.username = username;
+        this.userId = userId;
+    }
+}
