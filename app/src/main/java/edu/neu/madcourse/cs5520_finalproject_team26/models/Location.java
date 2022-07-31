174aeb10224102f26d47cfcf930588e73bdc4ddb
Avanti Dorle <avantidorle@avantis-air.myfiosgateway.com>
2022-07-31 16:45:45 -0400

    location model changed ro add question ids

--- app/src/main/java/edu/neu/madcourse/cs5520_finalproject_team26/models/Location.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_finalproject_team26/models/Location.java
@@ -1,18 +1,25 @@
 package edu.neu.madcourse.cs5520_finalproject_team26.models;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class Location {
-    public int locationId;
-    public String address;
-    public ArrayList<String> questionIds;
 
+    private final String location;
+    private final Map<String, String> questions;
 
-    public Location(int locationId, String address, ArrayList<String> questionIds) {
-        this.locationId = locationId;
-        this.address = address;
-        this.questionIds = questionIds;
+    public Location(String location, String questions) {;
+        this.location = location;
+        this.questions = new HashMap<>();
+        this.questions.put(UUID.randomUUID().toString(), questions);
     }
 
+    public String getLocation() {
+        return location;
+    }
 
+    public Map<String,String> getQuestions() {
+        return questions;
+    }
 }
