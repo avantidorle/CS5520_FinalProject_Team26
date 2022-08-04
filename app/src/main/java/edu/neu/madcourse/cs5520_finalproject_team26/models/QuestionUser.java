d4803c9ed5c161b89d3da4c4562eefbcbe00f97e
Pooja Nangude <poojanangude@Poojas-MacBook-Pro.local>
2022-08-03 23:54:22 -0400

    chang in QuestionUser model

--- app/src/main/java/edu/neu/madcourse/cs5520_finalproject_team26/models/QuestionUser.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_finalproject_team26/models/QuestionUser.java
@@ -1,15 +1,17 @@
 package edu.neu.madcourse.cs5520_finalproject_team26.models;
 
 public class QuestionUser {
-    String userId;
-    String questionId;
-    boolean vote;
-    boolean answer;
+    public String userId;
+    public String questionId;
+    public String vote;
+    public boolean answer;
+    public String userIdAndQuestionId;
 
-    public QuestionUser(String userId, String questionId, boolean vote, boolean answer) {
+    public QuestionUser(String userId, String questionId, String vote, boolean answer, String userIdAndQuestionId) {
         this.userId = userId;
         this.questionId = questionId;
         this.vote = vote;
         this.answer = answer;
+        this.userIdAndQuestionId = userIdAndQuestionId;
     }
 }
